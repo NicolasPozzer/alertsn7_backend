678151d2e3edbad98687535ee46e5cb5e4e00aab
NicolasPozzer <pozzernico@gmail.com>
2023-10-17 20:51:29 -0300

    first commit

---
+++ src/main/java/com/demo/alertsn7_backend/Alertsn7BackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.alertsn7_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alertsn7BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alertsn7BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/alertsn7_backend/Alertsn7BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.alertsn7_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alertsn7BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
